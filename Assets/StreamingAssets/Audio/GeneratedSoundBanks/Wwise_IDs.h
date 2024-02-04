44ffafb357c23ba10146b73f2c812ae222ee8c9c
TheAntilope <gaijiloic@gmail.com>
2024-02-04 16:19:42 -0500

    wwise fat pull

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,56 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_CRYSTAL_EMITTER = 1786550623U;
+        static const AkUniqueID PLAY_FOOTSTEP_MATERIAL = 1766743556U;
+        static const AkUniqueID PLAY_ICESCAPE_LEVEL = 916155420U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace PLAYER_FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 1730208058U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID SNOW = 787898836U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace PLAYER_FOOTSTEPS
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
